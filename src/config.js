2f2b72e1932f2c188c1f0b22ee939d7465cfd65f
Star-Owl <hasiradoo@starowl.dev>
2023-09-06 13:18:16 +0200

    Added comments to config file

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ const config = {
     postingMinute: 0,
     reminderHour: 14, // 24-hour format
     reminderMinute: 30,
-    reminderRemove: 5, //in seconds
-    token: process.env.BOT_TOKEN,
-    channelId: process.env.CHANNEL_ID,
-    authorId: process.env.AUTHOR_ID,
+    reminderRemove: 5, // Duration in seconds before the reminder is removed
+    token: process.env.BOT_TOKEN, // Your bot's token
+    channelId: process.env.CHANNEL_ID, // ID of the channel where the bot should post
+    authorId: process.env.AUTHOR_ID, // ID of the bot's author
 }
 
 export default config
