408b1744c138b5a3b596baca99d810901f5a7a24
Star-Owl <hasiradoo@starowl.dev>
2023-09-11 11:43:18 +0200

    Mineral Images from wikiAPI

--- src/functions/handleCommands.js
+++ src/functions/handleCommands.js
@@ -2,6 +2,7 @@ import { getRandomMineral } from './getMinerals.js'
 import { createMineralEmbed } from './createMineralEmbed.js'
 import getReminderMessage from './getReminderMessage.js'
 import config from '../config.js'
+import axios from 'axios'
 
 const getSecondsText = (seconds) => (seconds === 1 ? 'second' : 'seconds')
 
@@ -43,13 +44,34 @@ export const handleTimeCommand = async (receivedMessage) => {
     }, 1000)
 }
 
+async function getMineralImage(mineralName) {
+    const response = await axios.get(
+        `https://en.wikipedia.org/w/api.php?action=query&titles=${mineralName}&prop=pageimages&format=json&pithumbsize=500`
+    )
+
+    const pages = response.data.query.pages
+    const page = Object.values(pages)[0]
+
+    return page.thumbnail ? page.thumbnail.source : null
+}
+
+async function generateRandomMineralJson(mineral) {
+    const mineralImage = await getMineralImage(mineral.name)
+
+    mineral.image = mineralImage
+
+    return mineral
+}
+
 /**
  * Handle the mineral command.
  *
  * @param {string} message - The message content.
  * @param {object} author - The author of the message.
  */
-export const handleMineralCommand = (message, author, data) => {
+export const handleMineralCommand = async (message, author, data) => {
+    //await generateRandomMineralJson().then(console.log)
+    //chatCompletion()
     if (data.length === 0) {
         return message.channel.send({
             content: `Unknown Mineral\n\n This mineral is unknown because the database is currently empty.`,
@@ -57,11 +79,12 @@ export const handleMineralCommand = (message, author, data) => {
     }
 
     const randomMineral = getRandomMineral(data)
-    const mineralEmbed = createMineralEmbed(randomMineral, author)
+    const updatedMineral = await generateRandomMineralJson(randomMineral)
 
+    const mineralEmbed = createMineralEmbed(randomMineral, author)
     message.channel.send({
         content: `DEBUG: Daily Mineral: **${randomMineral.name}**\n\n ${randomMineral.description}`,
-        files: [randomMineral.image],
+        files: [updatedMineral.image], //randomMineral.image
         embeds: [mineralEmbed],
     })
 }
