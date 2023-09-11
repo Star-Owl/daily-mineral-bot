96fdfba880d613c0ce7c9b53dc9d808f99781455
Star-Owl <hasiradoo@starowl.dev>
2023-09-11 11:40:32 +0200

    Prepare for AI daily mineral

--- src/config.js
+++ src/config.js
@@ -7,6 +7,7 @@ const config = {
     token: process.env.BOT_TOKEN, // Your bot's token
     channelId: process.env.CHANNEL_ID, // ID of the channel where the bot should post
     authorId: process.env.AUTHOR_ID, // ID of the bot's author
+    openaiApiKey: process.env.OPENAI_API_KEY, // Your OpenAI API key
 }
 
 export default config
