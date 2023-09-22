3cdbfa76fdbdb0647cb6e7e20009fa39d4b8f336
Star-Owl <hasiradoo@starowl.dev>
2023-09-22 10:22:07 +0200

    updated config file

--- src/config.js
+++ src/config.js
@@ -1,13 +1,15 @@
 const config = {
-    postingHour: 15, // 24-hour format
+    postingHour: 13, // 24-hour format
     postingMinute: 0,
-    reminderHour: 14, // 24-hour format
+    reminderHour: 12, // 24-hour format
     reminderMinute: 30,
-    reminderRemove: 10, // Duration in seconds before the reminder is removed
+    reminderRemove: 10, // Duration in seconds before the reminder is removed (!time command)
     token: process.env.BOT_TOKEN, // Your bot's token
-    channelId: process.env.CHANNEL_ID, // ID of the channel where the bot should post
-    authorId: process.env.AUTHOR_ID, // ID of the bot's author
+    channelId: '1152687502654709802', // ID of the channel where the bot should post
+    authorId: '569975072417251378', // ID of the bot's author
     openaiApiKey: process.env.OPENAI_API_KEY, // Your OpenAI API key
+    roleId: '1151156562321227866', // ID of the role that should be mentioned when a mineral is posted
+    roleId_admin: '1140197709790335036', // ID of the admin role
 }
 
 export default config
